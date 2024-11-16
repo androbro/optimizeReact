9315d9822421a8d8d77d843c333dd58ef1313cb0
koen de vulder <devulderk@gmail.com>
2024-11-16 21:22:50 +0100

    feat: added tailwind

--- optimize-react/src/index.tsx
+++ optimize-react/src/index.tsx
@@ -7,6 +7,7 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import theme from './theme';
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
