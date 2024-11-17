d540b2b93628267e0ea7171a3a4b7449b485ff4f
koen de vulder <devulderk@gmail.com>
2024-11-17 17:20:21 +0100

    feat: added project structure

--- optimize-react/src/App.tsx
+++ optimize-react/src/App.tsx
@@ -1,40 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from '@mui/material'
+import HomePage from "./pages/HomePage.tsx";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-            <div className="p-4 bg-blue-500 text-white">
-                Tailwind CSS is working!
-            </div>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    <Button variant="contained">Hello world</Button>
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+           <HomePage />
         </>
     )
 }
---
+++ optimize-react/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import PageLayout from "../components/PageLayout.tsx";
+
+interface HomePageProps {
+}
+
+export default function HomePage({}: HomePageProps) {
+    return (
+        <PageLayout>
+            <h1>Home Page</h1>
+        </PageLayout>
+    );
+}
